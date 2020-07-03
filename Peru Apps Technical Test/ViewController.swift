b8d219517448cf28d451341b6d21ff55589e7e02
Nicolás A. Rodríguez <nrq5510@gmail.com>
2020-07-03 17:35:48 -0500

    Initial Commit

---
+++ Peru Apps Technical Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Peru Apps Technical Test
+//
+//  Created by Nicolás A. Rodríguez on 7/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Peru Apps Technical TestTests/Peru_Apps_Technical_TestTests.swift
@@ -0,0 +1,33 @@
+//
+//  Peru_Apps_Technical_TestTests.swift
+//  Peru Apps Technical TestTests
+//
+//  Created by Nicolás A. Rodríguez on 7/3/20.
+//
+
+import XCTest
+@testable import Peru_Apps_Technical_Test
+
+class Peru_Apps_Technical_TestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
